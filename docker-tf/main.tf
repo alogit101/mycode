24298c25635a94981b123bdf1a5f81af023d61f1
alogit101 <aalonso1977@gmail.com>
2024-09-09 20:15:46 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
+# My Name is Adrian Alonso
+// I am unable to scroll up
+
+/* I will have to settle for writing these comments on here.
+This is my final example.*/
+
+  
+
+
+
+
